0d719daa15c52460e7dc6116387b2bc9872b8a0a
EftiarRimon <bsse1356@iit.du.ac.bd>
2026-08-01 21:52:46 +0600

    more this prac

--- 01-Objects/object-this.js
+++ 01-Objects/object-this.js
@@ -8,3 +8,28 @@ const person={
 };
 
 document.getElementById("demo5").innerHTML=person.fullName();
+
+const person1={
+    firstName:"john",
+    lastName:"abraham",
+    id:"100",
+   myFunction: function() {
+    return this;
+   }
+};
+document.getElementById("demo4").innerHTML="this is " + person1.myFunction();
+
+const person4={
+    fullName:function(){
+        return this.firstName+ " " +this.lastName;
+    }
+}
+
+const person2={
+    firstName:"john",
+    lastName:"doe",
+}
+let x=person4.fullName.call(person2);
+
+document.getElementById("demo3").innerHTML=x;
+
