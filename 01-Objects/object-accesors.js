6667a767afd9936a7cc34aeba6784b8c5e0c3847
EftiarRimon <bsse1356@iit.du.ac.bd>
2026-08-11 22:33:20 +0600

    setGet Counter

--- 01-Objects/object-accesors.js
+++ 01-Objects/object-accesors.js
@@ -79,3 +79,41 @@ const club ={
 club.team="psg"
 
 document.getElementById("demo5").innerHTML=club.french;
+
+
+
+const obj={counter:0};
+
+Object.defineProperty(obj, "reset",{
+    get : function(){this.counter=0}
+});
+
+Object.defineProperty(obj, "increment",{
+    get:function(){this.counter++;}
+});
+
+Object.defineProperty(obj,"decrement",{
+    get:function(){this.counter--;}
+});
+
+Object.defineProperty(obj, "add",{
+    set:function(value){this.counter+=value;}
+});
+
+Object.defineProperty(obj,"subtract",{
+    set: function(value){this.counter-=value;}
+});
+
+obj.reset;
+obj.add=100;
+obj.subtract=9;
+obj.increment;
+obj.increment;
+obj.increment;
+obj.decrement;
+obj.decrement;
+obj.add=2;
+obj.subtract=3;
+
+
+document.getElementById("demo6").innerHTML=obj.counter;
