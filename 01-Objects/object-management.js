3907a7bfb36a9c4c585cf82740853a4c9a4116c1
EftiarRimon <bsse1356@iit.du.ac.bd>
2026-08-12 23:32:50 +0600

    definePropertyMethid()

--- 01-Objects/object-management.js
+++ 01-Objects/object-management.js
@@ -16,3 +16,18 @@ Object.defineProperty(person,"lastName", {value:"Ahmed"})
 
 document.getElementById("demo").innerHTML= person.
 firstName+" "+ person.lastName+ " is "+person.age+" years old";
+
+
+const player={
+    firstName : "Lionel",
+    lastName: "Messi"
+};
+Object.defineProperty(player, "fullName",
+    {
+        get: function(){
+            return this.firstName+" "+this.lastName;
+        }
+
+});
+
+document.getElementById("demo2").innerHTML=player.fullName;
