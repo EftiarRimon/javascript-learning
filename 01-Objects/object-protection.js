1afdc05d4e4d01920ab4e2ad05378dbbf542a801
EftiarRimon <bsse1356@iit.du.ac.bd>
2026-08-26 20:54:30 +0600

    array protection

--- 01-Objects/object-protection.js
+++ 01-Objects/object-protection.js
@@ -10,8 +10,21 @@ let text="";
 try {
     person.age="27";
 }
-catch (err){
-    text=err;
+catch (err1){
+    text=err1;
 }
 
 document.getElementById("demo1").innerHTML=text;
+
+const club = ["barca", "real madrid", "bayern", "M.city", "AC milan", "PSG", "Chelsea"];
+Object.preventExtensions(club);
+
+let text2="";
+try{
+    club.push("atm");
+}
+catch (err){
+    text2=err;
+};
+
+document.getElementById("demo2").innerHTML=text2;
