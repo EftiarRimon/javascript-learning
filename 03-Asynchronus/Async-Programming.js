bc38cab07429c4bbf731b2b4a57d09e226a76133
EftiarRimon <bsse1356@iit.du.ac.bd>
2026-08-29 14:58:27 +0600

    JS Function Seq

--- 03-Asynchronus/Async-Programming.js
+++ 03-Asynchronus/Async-Programming.js
@@ -1,6 +1,17 @@
 function myDisplayer(some){
-    document.getElementById("demo").innerHTML+=some+" ";
+    document.getElementById("demo").innerHTML+=some+"<br> ";
 }
 
 myDisplayer("A B C")
 myDisplayer("A")
+
+function myFirst(){
+    myDisplayer("This is 1");
+}
+
+function mySecond(){
+    myDisplayer("this is 2")
+}
+
+mySecond();
+myFirst();
