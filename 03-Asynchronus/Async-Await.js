333b41ea5f284d302139622cbb8f543fe032c895
EftiarRimon <bsse1356@iit.du.ac.bd>
2026-09-02 22:22:01 +0600

    AWAITs background things

--- 03-Asynchronus/Async-Await.js
+++ 03-Asynchronus/Async-Await.js
@@ -1,7 +1,8 @@
+//ASYNC FUNCTION
+
 function Displayer(text){
     document.getElementById("demo").innerHTML+=text+"<br>";
 }
-
 
 async function hello() {
     return "hello world!";
@@ -11,5 +12,39 @@ hello().then(function(value){
     Displayer(value)
 })
 
-
 Displayer("hi")
+
+// AWAIT FUNCTION
+
+function myDisplayer1(text1){
+    document.getElementById("demo1").innerHTML=text1;
+}
+
+async function getData() {
+    let response=await fetch("03-Asynchronus/fetch.txt");
+    let text=await response.text();
+    myDisplayer1(text);
+    
+}
+getData();
+
+
+//What Happens During await?
+
+
+function myDisplayer2(text2){
+    document.getElementById("demo2").innerHTML+=text2+"<br>";
+}
+
+myDisplayer2("Task: START --> ");
+
+async function GetData() {
+    await fetch("03-Asynchronus/fetch.txt");
+    myDisplayer2("Task: COMPLETED!");
+}
+
+GetData();
+
+myDisplayer2("CONTINIUE")
+
+
