f8041bd859034c3500af2073177e80009c100f1a
EftiarRimon <bsse1356@iit.du.ac.bd>
2026-09-16 20:59:58 +0600

    Async Debugging

---
+++ 03-Asynchronus/Async-Debugging.js
@@ -0,0 +1,13 @@
+
+//Debugging Start
+
+console.log("Before Fetch");
+
+const promise=fetch("03-Asynchronus/fetch.txt");
+
+console.log(promise);
+
+console.log("after fetch");
+
+
+//Async Debbugging
